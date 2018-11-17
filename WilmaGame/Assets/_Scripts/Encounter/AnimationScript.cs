54b6fb3c4108d755c6958e300727d290a10ea7f3
maukii <maunokiiskinen@kamk.fi>
2018-11-17 22:24:01 +0200

    Scripts foldiers and scripts added

---
+++ WilmaGame/Assets/_Scripts/Encounter/AnimationScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+
+    public AnimationCurve PositionCurve;
+    public AnimationCurve ScaleCurve;
+
+    public Vector2 startPos, endPos;
+    public Vector2 startScale, endScale;
+
+    public float effectTime = 1;
+
+    public bool ready = false;
+
+    void Start()
+    {
+        //SetupVariables(); // mayabe use later
+        StartEncounter();
+    }
+
+    void SetupVariables()
+    {
+        endPos = transform.localPosition;
+        endScale = transform.localScale;
+    }
+
+    public void StartEncounter()
+    {
+        StartCoroutine(Animation());
+    }
+
+    IEnumerator Animation()
+    {
+        ready = false;
+
+        transform.localPosition = startPos;
+        transform.localScale = startScale;
+
+        float time = 0;
+        float percent = 0;
+        float lastTime = Time.realtimeSinceStartup;
+
+        do
+        {
+            time += Time.realtimeSinceStartup - lastTime;
+            lastTime = Time.realtimeSinceStartup;
+
+            percent = Mathf.Clamp01(time / effectTime);
+            Vector2 tempScale = Vector2.LerpUnclamped(startScale, endScale, ScaleCurve.Evaluate(percent));
+            Vector2 tempPos = Vector2.LerpUnclamped(startPos, endPos, PositionCurve.Evaluate(percent));
+
+            transform.localPosition = tempPos;
+            transform.localScale = tempScale;
+            yield return null;
+
+        } while (percent < 1);
+
+        transform.localScale = endScale;
+        transform.localPosition = endPos;
+        ready = true;
+    }
+
+}
---
+++ WilmaGame/Assets/_Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public static bool interacting = false;
+
+    [SerializeField] bool canMoveDiagonally = false;
+    [SerializeField] float movementSpeed = 3f;
+
+    public float inputV { get; private set; }
+    public float inputH { get; private set; }
+
+    public bool movingV = false, movingH = false;
+
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+        RotatePlayer();
+        UpdateAnimations();
+    }
+
+    private void Movement()
+    {
+        if (!interacting)
+        {
+            inputH = Input.GetAxisRaw("Horizontal");
+            inputV = Input.GetAxisRaw("Vertical");
+
+            if (canMoveDiagonally)
+            {
+                Vector3 direction = new Vector3(inputH, inputV, 0);
+
+                if (!interacting)
+                {
+                    transform.Translate(direction * movementSpeed * Time.deltaTime);
+                }
+            }
+            else
+            {
+                if (inputH != 0 && !movingV)
+                {
+                    movingH = true;
+
+                    Vector3 direction = new Vector3(inputH, 0, 0);
+                    transform.Translate(direction * movementSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    movingH = false;
+                }
+
+                if (inputV != 0 && !movingH)
+                {
+                    movingV = true;
+
+                    Vector3 direction = new Vector3(0, inputV, 0);
+                    transform.Translate(direction * movementSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    movingV = false;
+                }
+            }
+        }
+        else
+        {
+            inputH = 0f;
+            inputV = 0f;
+
+            anim.SetFloat("Hor", 0);
+            anim.SetFloat("Ver", 0);
+        }
+    }
+
+    private void RotatePlayer()
+    {
+        if (inputH < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (inputH > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    private void UpdateAnimations()
+    {
+        anim.SetFloat("Hor", inputH);
+        anim.SetFloat("Ver", inputV);
+    }
+}
