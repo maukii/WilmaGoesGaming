94e4134f83f9ed3b3de15483d7f094c4ebd62b73
RiikkaKilp <RiikkaKilp@users.noreply.github.com>
2018-11-19 23:14:50 +0200

    battle transition

---
+++ WilmaGame/Assets/_Scripts/CameraBattleTransition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBattleTransition : MonoBehaviour {
+
+    public Material TransitionMat;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, TransitionMat);
+    }
+}
